e80d92bd731e7d19a06ce91cf404be293450ba00
Wei Wu <william.third.wu@gmail.com>
2021-01-15 20:15:28 +0100

    Create compile.py

---
+++ Graph_Network Embedding/HashGNNPlus/#GNN+/compile.py
@@ -0,0 +1,5 @@
+import os
+
+method = 'hashgnnplus'
+os.system("rm "+method);
+os.system("g++ -std=c++11 -lm -O3 -march=native -Wall -funroll-loops -ffast-math -Wno-unused-result "+method+".cpp -o "+method+" -lgsl -lm -lgslcblas");
